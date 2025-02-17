232fbccd61845ab77698c898997577166f6524e5
Lee-gi-yeun <leessjjgg123456@naver.com>
2025-02-17 11:41:13 +0900

    Java

--- chap01-literal-and-variable-lecture-source/src/main/java/com/ohgiraffers/section01/literal/Application02.java
+++ chap01-literal-and-variable-lecture-source/src/main/java/com/ohgiraffers/section01/literal/Application02.java
@@ -3,6 +3,7 @@ package com.ohgiraffers.section01.literal;
 public class Application02 {
     public static void main(String[] args) {
 
+        System.out.println("=== 정수와 정수 ====");
         System.out.println(123 + 456);
         System.out.println(123 - 23);
         System.out.println(123 * 10);
@@ -27,5 +28,49 @@ public class Application02 {
         System.out.println('a' * 'b');
         System.out.println('a' / 'b');
         System.out.println('a' % 'b');
+
+        System.out.println('a' + 1);
+        System.out.println('a' - 1);
+        System.out.println('a' * 2);
+        System.out.println('a' / 2);
+        System.out.println('a' % 2);
+
+        System.out.println("hello" + "world");
+//System.out.println("hello" - "world");			//에러 발생
+//System.out.println("hello" * "world");			//에러 발생
+//System.out.println("hello" / "world");			//에러 발생
+//System.out.println("hello" % "world");			//에러 발생
+
+        System.out.println("====문자열과 다른 리터럴 ====");
+        System.out.println("helloworld" + 123);
+        System.out.println("helloworld" + 123.456);
+        System.out.println("helloworld" + 'a');
+        System.out.println("helloworld" + true);
+        System.out.println("123" + "456");
+
+        //System.out.println(true + false);				//에러 발생
+//System.out.println(true - false);				//에러 발생
+//System.out.println(true * false);				//에러 발생
+//System.out.println(true / false);				//에러 발생
+//System.out.println(true % false);				//에러 발생
+
+        //System.out.println(true + 1);				//에러 발생
+//System.out.println(true - 1);				//에러 발생
+//System.out.println(true * 1);				//에러 발생
+//System.out.println(true / 2);				//에러 발생
+//System.out.println(true % 2);				//에러 발생
+
+        //System.out.println(true + 1.0);				//에러 발생
+//System.out.println(true - 1.0);				//에러 발생
+//System.out.println(true * 1.0);				//에러 발생
+//System.out.println(true / 2.0);				//에러 발생
+//System.out.println(true % 2.0);				//에러 발생
+
+        System.out.println(true + "a");		  		//문자열 합치기 됨
+//System.out.println(true - "a");				//에러 발생
+//System.out.println(true * "a");				//에러 발생
+//System.out.println(true / "a");				//에러 발생
+//System.out.println(true % "a");				//에러 발생
+
     }
 }
