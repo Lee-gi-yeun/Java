e5b5d3dada4a3bd977b20839fb069e944f1c36c7
Lee-gi-yeun <leessjjgg123456@naver.com>
2025-02-19 09:44:06 +0900

    Java

---
+++ chap04-control-lecture-source/src/main/java/com/ohgiraffers/section01/conditional/Application.java
@@ -0,0 +1,18 @@
+package com.ohgiraffers.section01.conditional;
+
+public class Application {
+    public static void main(String[] args) {
+        A_if a_if = new A_if();
+        B_ifElse b_ifElse = new B_ifElse();
+        C_ifElseIf cIfElseIf = new C_ifElseIf();
+
+//        a_if.testSimpleIfStatement();
+//        a_if.testNestedIfStatement();
+
+//        b_ifElse.testSimpleIfElseStatement();
+
+//        cIfElseIf.testIfElseIfStatement();
+//        cIfElseIf.testNestedIfElseIfStatement();
+        cIfElseIf.improveIfElseIfStatement();
+    }
+}
