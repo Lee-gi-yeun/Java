4e5b0695234fbe6bcd10b3a0655a2f719d1e94f4
Lee-gi-yeun <leessjjgg123456@naver.com>
2025-02-19 14:03:54 +0900

    Java

--- chap04-control-lecture-source/src/main/java/com/ohgiraffers/section03/branching/Application.java
+++ chap04-control-lecture-source/src/main/java/com/ohgiraffers/section03/branching/Application.java
@@ -4,8 +4,13 @@ public class Application {
     public static void main(String[] args) {
 
         A_break aBreak = new A_break();
+        B_continue bContinue = new B_continue();
 
 //        aBreak.testSimpleBreakStatement();
-        aBreak.testSimpleBreakStatement2();
+//        aBreak.testSimpleBreakStatement2();
+        aBreak.testJumpBreak();
+//        bContinue.testSimpleContinueStatement();
+//        bContinue.testSimpleContinueStatement2();
+//        bContinue.testJumpContinue();
     }
 }
