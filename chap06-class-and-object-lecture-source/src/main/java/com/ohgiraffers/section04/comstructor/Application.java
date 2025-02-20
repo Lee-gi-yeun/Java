b1c5b1573a4988598f44a7e80cc6a7e62398b030
Lee-gi-yeun <leessjjgg123456@naver.com>
2025-02-20 14:48:51 +0900

    Java

---
+++ chap06-class-and-object-lecture-source/src/main/java/com/ohgiraffers/section04/comstructor/Application.java
@@ -0,0 +1,8 @@
+package com.ohgiraffers.section04.comstructor;
+
+public class Application {
+    public static void main(String[] args) {
+        // 생성자 함수가 무엇인지 이해하고 선언 및 호출할 수 있다
+        User user = new User();
+    }
+}
