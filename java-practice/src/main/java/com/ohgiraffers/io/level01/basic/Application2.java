7daea2e7afe94d48ba57d884ecd4baf0bc209ab2
Lee-gi-yeun <leessjjgg123456@naver.com>
2025-02-25 17:00:58 +0900

    java-practice

--- java-practice/src/main/java/com/ohgiraffers/io/level01/basic/Application2.java
+++ java-practice/src/main/java/com/ohgiraffers/io/level01/basic/Application2.java
@@ -1,5 +1,11 @@
 package com.ohgiraffers.io.level01.basic;
 
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Scanner;
+
 public class Application2 {
     public static void main(String[] args) {
         /* ----- 입력 예시 -----
@@ -23,6 +29,22 @@ public class Application2 {
          * 오류 : origin2.txt (지정된 파일을 찾을 수 없습니다)
          *
          */
+        Scanner sc = new Scanner(System.in);
+        System.out.print("원본 파일의 이름을 입력하세요 : ");
+        String origin = sc.nextLine();
+        System.out.print("복사 파일의 이름을 입력하세요 : ");
+        String copy = sc.nextLine();
+
+        try(FileReader fr = new FileReader(origin)){
+                try(FileWriter fw = new FileWriter(copy)){
+                    int data;
+                    while ((data = fr.read()) != -1) {
+                        fw.write(data);
+                    }
+                }
+        } catch (IOException e) {
+            System.out.printf("오류 : "+origin+" (지정된 파일을 찾을 수 없습니다.)");
+        }
 
     }
 }
