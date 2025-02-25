7996b6ce3e66aa9dacbf00d01466d393b4e2a59e
Lee-gi-yeun <leessjjgg123456@naver.com>
2025-02-25 11:56:52 +0900

    Java

---
+++ chap10-exception-lecture-source/src/main/java/com/ohgiraffers/section02/userexception/Application.java
@@ -0,0 +1,8 @@
+package com.ohgiraffers.section02.userexception;
+
+public class Application {
+    public static void main(String[] args) {
+        // 사용자 정의 예외 클래스를 사용할 수 있다
+        ExceptionTest exceptionTest = new ExceptionTest();
+    }
+}
